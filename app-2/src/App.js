a0c44d563203d1e7c2fc6c4821d8abbc6c518c01
OGCypherpunk <chuck.devmtn@gmail.com>
2018-03-19 20:10:31 -0500

    completed 1&2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dreamCars : [
+        'Ferrari',
+        'Lamborghini',
+        'Koenigsegg',
+        'Mclaren',
+        'Tesla'
+      ]
+    }
+  }
+
+
   render() {
+    let displayCars = this.state.dreamCars.map( (element, index) => {
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { displayCars }
       </div>
     );
+
   }
 }
+    
+  
 
 export default App;
